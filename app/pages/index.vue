baaf15b493b01fc731c4781efaa023caf0a0f011
onishi.kohei <kohei.o2222@gmail.com>
2022-04-17 14:36:57 +0900

    tailwind追加

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <div class='bg-gray-500'>test</div>
+  </div>
+</template>
---
+++ app/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './app.vue',
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
